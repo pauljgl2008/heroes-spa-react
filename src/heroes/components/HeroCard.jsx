017eb73f3131b22c3a3843ede95a784bdcbba85d
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-21 12:27:00 -0500

    feat: HeroCard components part 2

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+const CharactersByHero = ({ alter_ego, characters }) => {
+    // if (alter_ego === characters) return (<></>);
+    // return (<p>{characters}</p>);
+    return (alter_ego === characters)
+        ? (<></>)
+        : (<p>{characters}</p>);
+}
+
 export const HeroCard = ({
     id,
     superhero,
@@ -7,6 +16,7 @@ export const HeroCard = ({
     characters,
 }) => {
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
+    // const charactersByHero = (<p>{characters}</p>);
     return (
         <>
             <div className="col">
@@ -19,7 +29,13 @@ export const HeroCard = ({
                             <div className="card-body">
                                 <h5 className="card-title">{superhero}</h5>
                                 <p className="card-text">{alter_ego}</p>
-                                <p>{characters}</p>
+                                <CharactersByHero characters={characters} alter_ego={alter_ego} />
+                                <p className="card-text">
+                                    <small className="text-muted">{first_appearance}</small>
+                                </p>
+                                <Link to={`./hero/${id}`}>
+                                    Más...
+                                </Link>
                             </div>
                         </div>
                     </div>
