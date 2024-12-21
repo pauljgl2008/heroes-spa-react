a1fa86fcb6b6efa4d41a70f4e897a4cee5be8718
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-21 10:05:33 -0500

    feat: class active route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,12 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { MarvelPage } from "../heroes/pages/MarvelPage"
 import { DcPage } from "../heroes/pages/DcPage"
 import { LoginPage } from "../auth/pages/LoginPage"
+import { Navbar } from "../ui"
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar/>
             <Routes>
                 <Route path="/marvel" element={<MarvelPage />} />
                 <Route path="/dc" element={<DcPage />} />
