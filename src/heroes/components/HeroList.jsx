60dad44a570ed10906bfdbb7f76e2ac2c45bfada
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-21 12:29:21 -0500

    fix: HeroList style

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -4,7 +4,7 @@ import { HeroCard } from "./HeroCard";
 export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher(publisher);
     return (
-        <div className="row rows-cols-1 rows-cols-md-3 g-3">
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
             {
                 heroes.map(hero => (
                     <HeroCard
