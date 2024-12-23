16ae67e2793c00fcf745335ecd330ac1ae9383ac
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-23 17:17:30 -0500

    feat: design of search screen, SearchPage

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -31,6 +31,12 @@ export const Navbar = (props) => {
                     >
                         DC
                     </NavLink>
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
