d51cba2dc854d734e08bf133256729ca0f70c021
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-23 16:30:00 -0500

    feat: add styles for HeroPage and navigate back

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,10 +1,13 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
     const { id } = useParams();
     const hero = getHeroById(id);
-
+    const navigate = useNavigate();
+    const onNavigateBack = () => {
+        navigate(-1);
+    }
     if (!hero) {
         return <Navigate to="/marvel" />
     }
@@ -25,6 +28,13 @@ export const HeroPage = () => {
                         <li className="list-group-item"><b>Publisher:</b> {hero.publisher} </li>
                         <li className="list-group-item"><b>First appearance:</b> {hero.first_appearance} </li>
                     </ul>
+                    <h5 className="mt-3"> Characters </h5>
+                    <p>{hero.characters}</p>
+                    <button
+                        className="btn btn-outline-info"
+                        onClick={onNavigateBack}>
+                        Regresar
+                    </button>
                 </div>
             </div>
         </>
