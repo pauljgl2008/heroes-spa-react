01147b5285601bab1765cf9c9d2d43c46ad4b9bd
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-23 20:02:52 -0500

    feat: keep active user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -7,17 +7,30 @@ const initialState = {
     logged: false
 }
 
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user: user
+    }
+}
+
 export const AuthProvider = ({ children }) => {
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
     const login = (name = '') => {
+        const user = {
+            id: '123',
+            name
+        }
         const action = {
             type: types.login,
-            payload: {
-                id: '123',
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify(user));
+
         dispatch(action);
     }
     return (
