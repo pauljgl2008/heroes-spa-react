75f2eb85d25a777fa334ac945985c952e034d8e0
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-23 20:49:25 -0500

    ffefat: public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { HeroesRoutes } from "../heroes/"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute.jsx"
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                {/* <Route path="/login" element={<LoginPage />} /> */}
 
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
                 <Route path="/*" element={
                     <PrivateRoute>
                         <HeroesRoutes />
